4e908863ac5a416315d5f9df5cb364c4f9725a1a
cymon337 <cymon337@gmail.com>
2023-08-28 23:08:31 +0900

    # feature : -  Protect few routes so that only authorized user / admin can have access to it by adding new AuthMiddleware

--- build-rest-apis/src/routes/authors.ts
+++ build-rest-apis/src/routes/authors.ts
@@ -2,6 +2,8 @@ import { FileUploader } from '../http/middlewares/FileUploader';
 import { ErrorHandler } from '../http/middlewares/ErrorHandler';
 import { AuthorsController } from '../http/controllers/AuthorsController';
 import express from "express";
+import { AuthMiddleware } from '../http/middlewares/AuthMiddleware';
+import { AdminMiddleware } from '../http/middlewares/AdminMiddleware';
 
 
 const authorsController = new AuthorsController();
@@ -14,17 +16,23 @@ router.get("/:id", ErrorHandler.catchErrors(authorsController.getAuthor));
 
 router.post(
     "/", 
+    ErrorHandler.catchErrors(AuthMiddleware.authenticate),
+    ErrorHandler.catchErrors(AdminMiddleware.check),
     FileUploader.upload("image", "authors", 2 * 1024 * 1024), 
     ErrorHandler.catchErrors(authorsController.create),
 );
 
 router.put(
     "/:id", 
+    ErrorHandler.catchErrors(AuthMiddleware.authenticate),
+    ErrorHandler.catchErrors(AdminMiddleware.check),
     ErrorHandler.catchErrors(authorsController.update),
 );
 
 router.delete(
     "/:id",
+    ErrorHandler.catchErrors(AuthMiddleware.authenticate),
+    ErrorHandler.catchErrors(AdminMiddleware.check),
     ErrorHandler.catchErrors(authorsController.delete),
 );
 
